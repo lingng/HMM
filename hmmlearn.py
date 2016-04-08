001d34be56f033b5f902f3c6bd3a9f4d384b506b
Yuting Zhang <lizlingng@gmail.com>
2016-04-08 16:05:32 -0700

    Finish constructing the model and write into the model file.

--- hmmlearn.py
+++ hmmlearn.py
@@ -10,7 +10,92 @@ yutingz@usc.edu
 
 import sys
 import os
-import re
+# import codecs
+import json
+
+# Global
+transition_dic = {}		#key: tag1,tag2  value: count
+start_tag_dic = {}		#key: tag_start  value: count
+
+tag_count_dic = {}		#key: tag 		value: count
+word_dic = {}			#key: word 		value: list of the count for the 29 tags
+
+tag_dic = { 			#key: tag 		value: index for the tag
+	'DI': 0, 'NC': 1, 'FF': 2, 'SP': 3, 
+	'DA': 4, 'AQ': 5, 'CC': 6, 'PR': 7, 
+	'VM': 8, 'VS': 9, 'ZZ': 10, 'P0': 11, 
+	'PP': 12, 'RG': 13, 'AO': 14, 'PX': 15, 
+	'NP': 16, 'CS': 17, 'VA': 18, 'DD': 19, 
+	'RN': 20, 'WW': 21, 'PI': 22, 'PD': 23, 
+	'PT': 24, 'DR': 25, 'DP': 26, 'DT': 27, 'II': 28
+}
+
+"""
+	Get the index for the corresponding tag.
+	
+	@tag: String
+	rtn: integer
+"""
+def get_idx(tag):
+	return tag_dic[tag]
+
+"""
+	Construc the model line by line.
+
+	@line: String of the labeled line
+"""
+def construct_model(line):
+	line = line.strip()
+	lst = line.split(' ')
+	slst = []
+	slst.append("q0")
+
+	for item in lst:
+		word = item.split('/')[:-1]
+		tag = item.split('/')[-1]
+		# print idx(tag)
+
+		if tag_count_dic.has_key(tag):
+			tag_count_dic[tag] += 1
+		else:
+			tag_count_dic[tag] = 1
+		slst.append(tag)
+
+		# Get the word part for the word/tag item. 
+		# The word may contain multiple '/', thus need to join it.
+		if len(item.split('/')) != 2:
+			key = '/'.join(item.split('/')[:-1])
+		else:
+			key = item.split('/')[0]
+		# Construct the word dictionary. Format: {'word': [list of the count for the word for each tag]}
+		if word_dic.has_key(key):
+			tidx = get_idx(tag)
+			word_dic[key][tidx] += 1
+		else:
+			taglst = [0]*29
+			tid = get_idx(tag)
+			taglst[tid] += 1
+			word_dic[key] = taglst
+
+	for i in range(0, len(slst)-2):
+		key = slst[i]+","+slst[i+1]
+		if transition_dic.has_key(key):
+			transition_dic[key] += 1
+		else:
+			transition_dic[key] = 1
+
+"""
+	Count the start tag of the transition to calculate the transition probability
+
+	@dic: The transition probability dictionary
+"""
+def count_start_tag(dic):
+	for key, value in dic.iteritems():
+		start_tag = key.split(',')[0]
+		if start_tag_dic.has_key(start_tag):
+			start_tag_dic[start_tag] += value
+		else:
+			start_tag_dic[start_tag] = value
 
 """
 Main Function
@@ -19,33 +104,37 @@ Main Function
 if len(sys.argv) != 2:
     print "Wrong argument number"
     sys.exit()
-
-# Get input file path
 path = sys.argv[1]
-# print path
-transition_dic = {}
+
+if not path.endswith('.txt'):
+	print "Please enter a path to the traning txt file"
+	sys.exit()
+
 
 with open(path, 'r') as fin:
 	while 1:
 		line = fin.readline()
 		if not line:
 			break
-		# print line
-		line = line.strip()
-		lst = line.split(' ')
-		slst = []
-		slst.append("q0")
-		# print lst
-		for item in lst:
-			tag = item.split('/')[-1]
-			# print item, tag
-			slst.append(tag)
-		# print slst
-		for i in range(0, len(slst)-2):
-			key = slst[i]+","+slst[i+1]
-			if transition_dic.has_key(key):
-				transition_dic[key] += 1
-			else:
-				transition_dic[key] = 1
-print transition_dic
-print len(transition_dic)
+		construct_model(line)
+
+count_start_tag(transition_dic)
+
+# Write dictionaries to model file.
+with open('hmmmodel.txt', 'w') as fout:
+	tmpstr = json.dumps(word_dic,ensure_ascii=False)
+	fout.write(tmpstr)
+	fout.write('\n')
+	tmpstr = json.dumps(tag_count_dic,ensure_ascii=False)
+	fout.write(tmpstr)
+	fout.write('\n')
+	tmpstr = json.dumps(start_tag_dic,ensure_ascii=False)
+	fout.write(tmpstr)
+	fout.write('\n')
+	tmpstr = json.dumps(transition_dic,ensure_ascii=False)
+	fout.write(tmpstr)
+
+# print len(word_dic)
+# print len(start_tag_dic)
+# print transition_dic
+# print len(tag_count_dic)
